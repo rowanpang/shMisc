e5e9e5a556a79c7556530b5072a1a1c94590d8e5
Rowan Pang <pangweizhen.2008@hotmail.com>
2019-02-20 10:39:54 +0800

    commit 2

--- ipswap.sh
+++ ipswap.sh
@@ -4,23 +4,32 @@ netA="
 192.168.17.1
 192.168.17.2
 192.168.17.3
+192.168.17.4
 192.168.17.5
 192.168.17.6
 "
+netA="
+192.168.17.1
+""
 
 netB="
 192.168.20.1
 192.168.20.2
 192.168.20.3
+192.168.20.4
 192.168.20.5
 192.168.20.6
 "
+netB="
+192.168.20.1
+""
 
-dir="AtB"
-
-sshCon="$netA"
-toChange="$netB"
-
+netMA="
+192.168.17.x
+"
+netMB="
+192.168.120.
+"
 function sshChk(){
     toChk=$1
     for cli in $toChk;do
@@ -35,11 +44,37 @@ function sshChk(){
 
 
 function doChange(){
-    toCon=$1
-    toChange=$2
+    toCons=$1
+    toChanges=$2
+    netM=$netMB
 
-    for cli in $sshCon;do
+    i=0
+    for cli in $toCons;do
+	((i+=1))
+	toCh=$(echo $toChanges | awk "{print \$$i}")
+	sfix=${toCh##*.}
+
 	ssh $cli systemctl restart NetworkManager
+
+	inf=`ssh $cli 'ip a' | grep "$toCh" | awk '{print $8}'`
+	link=`ssh $cli 'nmcli' | grep "connected to $inf" | sed 's/: connect.*//'`
+
+	mdgw=`ssh $cli 'ip r' | grep default | grep -c $link`
+	if [ $mdgw -ge 1 ];then
+	    ipgw=`ssh $cli 'ip r' | grep default | awk '{print $3}'`
+	    gwSfix=${ipgw##*.}
+	fi
+
+	echo "$cli: $toCh->$netM,$inf,$link,$mdgw"
+
+	#do modify
+	if [ X$debug == X ];then
+	    ssh $cli "nmcli connection modify $link ipv4.address $netM.$sfix/24"
+	    if [ $mdgw -ge 1 ];then
+		ssh $cli "nmcli connection modify $link ipv4.gateway $netM.$gwSfix"
+	    fi
+	    ssh $cli "nmcli connection down $link;nmcli connnection up $link"
+	fi
 
     done
 }
@@ -49,4 +84,5 @@ function main(){
     sshChk "$netB"
 }
 
+debug="yes"
 main
